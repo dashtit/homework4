e4541007111d17104b18733f55e531a5b551ea7c
dashtit <d.titowecz@yandex.ru>
2025-09-23 15:39:19 +0300

    fix unnecessary logic in remove_prev_symbol solution

--- homeworks/hw9/remove_prev_symbol/hw9_solution.py
+++ homeworks/hw9/remove_prev_symbol/hw9_solution.py
@@ -2,10 +2,10 @@ def remove_previous_symbol(raw_str: str) -> str:
     if raw_str == '':
         return raw_str
     split_str = []
-    for i, symb in enumerate(raw_str):
+    for _, symb in enumerate(raw_str):
         if symb != '#':
             split_str.append(symb)
-        if i < len(raw_str) - 1 and raw_str[i + 1] == '#':
+        else:
             if split_str:
                 split_str.pop()
     return ''.join(split_str)
