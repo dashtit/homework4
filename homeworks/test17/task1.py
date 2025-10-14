302049ffe2d48a72a410dfeae356fa88e9d1ea6e
dashtit <d.titowecz@yandex.ru>
2025-10-14 19:23:07 +0300

    add task1 solution

---
+++ homeworks/test17/task1.py
@@ -0,0 +1,10 @@
+some_string='This is a long long string'
+
+
+print(some_string[0])
+print(some_string[-1])
+print(some_string[2])
+print(some_string[-3])
+print(len(some_string))
+print(some_string[::-1])
+print(some_string[0:7])
