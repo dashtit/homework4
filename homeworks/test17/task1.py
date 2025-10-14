6d2d6e11e7e3fd4887ebce968ca9877561918f6e
dashtit <d.titowecz@yandex.ru>
2025-10-14 19:34:11 +0300

    fix asserts in task1

--- homeworks/test17/task1.py
+++ homeworks/test17/task1.py
@@ -1,10 +1,10 @@
 some_string='This is a long long string'
 
 
-print(some_string[0])
-print(some_string[-1])
-print(some_string[2])
-print(some_string[-3])
-print(len(some_string))
-print(some_string[::-1])
-print(some_string[0:7])
+assert some_string[0] == 'T'
+assert some_string[-1] == 'g'
+assert some_string[2] == 'i'
+assert some_string[-3] == 'i'
+assert len(some_string) == 26
+assert some_string[::-1] == 'gnirts gnol gnol a si sihT'
+assert some_string[0:7] == 'This is'
