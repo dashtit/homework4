8fb2179209aecf12c0ee0a719018ce1a72facacf
dashtit <d.titowecz@yandex.ru>
2025-11-07 00:06:38 +0300

    add test cases for saucedemo

---
+++ homeworks/hw23/test_saucedemo.py
@@ -0,0 +1,43 @@
+"""
+1. Go to the login page: https://www.saucedemo.com/
+    1.1 Fill 'Username' field with the value 'standard_user' -
+        selector for 'Username' field: input[id="user-name"]
+    1.2 Fill 'Password' field with the value 'secret_sauce' -
+        selector for 'Password' field: input[id="password"]
+    1.3 Press 'Login' button -
+        selector for 'Login' button: input[id="login-button"]
+
+2. On the opened page of the shop - https://www.saucedemo.com/inventory.html:
+    2.1 Press 'Add to cart' button for 'Sauce Labs Backpack' item -
+        selector for 'Add to cart' button: button[id="add-to-cart-sauce-labs-backpack"]
+    2.2 Press the Cart Icon in the upper right corner -
+        selector for 'Card' button: [class=shopping_cart_link]
+
+3. On the opened page of the cart - https://www.saucedemo.com/cart.html:
+    3.1 Press 'Checkout' button -
+        selector for 'Checkout' button: button[id="checkout"]
+
+4. On the opened page for checkout - https://www.saucedemo.com/checkout-step-one.html:
+    4.1 Fill 'First Name' field with the value 'Umpa' -
+        selector for 'First Name' field: input[id="first-name"]
+    4.2 Fill 'Last Name' field with the value 'Lumpa' -
+        selector for 'Last Name' field: input[id="last-name"]
+    4.3 Fill 'Zip/Postal Code' field with the value '123456' -
+        selector for 'Zip/Postal Code' field: input[id="postal-code"]
+    4.4 Press 'Continue' button -
+        selector for 'Continue' button: input[id="continue"]
+
+5. On the opened page of checkout overview - https://www.saucedemo.com/checkout-step-two.html:
+    5.1 Press 'Finish' button -
+        selector for 'Finish' button: button[id="finish"]
+
+6. On the opened page of completed checkout - https://www.saucedemo.com/checkout-complete.html
+    6.1 Press 'Back Home' button -
+        selector for 'Back Home' button: button[id="back-to-products"]
+
+7. On the opened page of the shop - https://www.saucedemo.com/inventory.html:
+    7.1 Press Menu Icon in the upper left corner -
+        selector for 'Menu' button: button[id="react-burger-menu-btn"]
+    7.1 Press 'Logout' button  on the opened Menu -
+        selector for 'Logout' button: [id="logout_sidebar_link"]
+"""
